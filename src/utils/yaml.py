8f324ef93d81f9ca9e9f6fb576a75c74f556eeac
Antônio H. Rodrigues <ahmoura1995@gmail.com>
2025-09-03 02:17:10 +0200

    feature 2, transform complete

--- src/utils/yaml.py
+++ src/utils/yaml.py
@@ -1,18 +1,20 @@
 import yaml
 
+
 # caminho com os links/caminhos para os dados a serem extraidos
 endpoint_path = {
     "file": 'src/utils/endpoint.yaml', #TODO: usar caminho relativo
     "mode": 'r'
 }
 
-def read_yaml(path:str, mode:str):
+
+def read_yaml(path:str, mode:str)->dict:
     with open(path, mode) as f:
         endpoint = yaml.safe_load(f)
-
     return endpoint
 
-def generate_url(endpoint_path:dict=endpoint_path): #TODO usar KWARGS
+
+def generate_url(endpoint_path:dict=endpoint_path)->(str,str,dict): #TODO usar KWARGS
     # gerando o links a partir do yaml famil
     endpoint_params = read_yaml(endpoint_path.get("file"), endpoint_path.get("mode"))
     protocol =  endpoint_params["api"]["protocol"]
@@ -21,5 +23,4 @@ def generate_url(endpoint_path:dict=endpoint_path): #TODO usar KWARGS
     path = endpoint_params["api"]["endpoints"]["get"]["path"]
     params =  endpoint_params["api"]["endpoints"]["get"]["params"]
     url = f'{protocol}://{host}{path}'
-
     return url, params, endpoint_params
