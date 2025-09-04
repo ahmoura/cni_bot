5c1ba767e992724aa62a6804dd928ce8491d1093
Antônio H. Rodrigues <ahmoura1995@gmail.com>
2025-09-04 11:00:12 +0200

    atualizando comentarios de funcoes e respondendo tarefa2

--- src/bot/etl.py
+++ src/bot/etl.py
@@ -3,9 +3,20 @@ import pandas as pd
 from uuid import uuid4
 from datetime import datetime
 
-# funcao para extrair os dados da url lida do arquivo de configuracao
 def extract(url:str, params:dict)->dict:
-    """TODO DOCS"""
+    """
+    Extrai dados de uma URL, tratando possiveis erros de requisicao.
+
+    Esta funcao faz uma requisicao HTTP GET para a URL fornecida
+    e retorna o conteudo JSON da resposta em caso de sucesso.
+
+    :param url: A URL para a qual a requisicao sera feita.
+    :type url: str
+    :param params: Parametros para a requisicao.
+    :type params: dict
+    :return: Um dicionario contendo a resposta JSON e o codigo de status HTTP.
+    :rtype: dict
+    """
     try:
         response = get(url, params=params, timeout=10)
         response.raise_for_status()
@@ -19,15 +30,28 @@ def extract(url:str, params:dict)->dict:
 
     
 
-# funcao recursiva baseada no tipo de dado que foi lido no payload
-# cada chamada recursiva passa o nome da chave fonte para refenciar no arquivo de saida
-# se for um dicionario, recursao
-# se for uma lista, salva o df na variavel output
-# caso contrario, armazena em uma lista (aqui dict) para salvar ao final da execucao
-# para o ultimo loop salva os dados da camada mais baixa em um arquivo chamdo IBGE
-    #TODO: passar todos parametros como esse para o arquivo params e ler eles usando as funcoes do yaml
-# TODO: poderia ser feito um trabalho mais eficiente na validacao dos dados, por exemplo os dados do campo "territorio", diversos arquivos foram criados com apenas uma linha. Nessa etapa ou apos a criacao de todos eles um tratamento para torna-los uma tabela unica com uma coluna identificando a key seria mais eficiente e organizado.
+ 
 def transform_by_type(data:dict, src_key:str = None):
+    """
+    Transforma dados aninhados de um dicionario em DataFrames do Pandas.
+
+    Esta e uma funcao recursiva que percorre um dicionario aninhado.
+    Se um valor for um dicionario, a funcao chama a si mesma. 
+    Se for uma lista, ela cria um DataFrame e o armazena. 
+    Se for um tipo de dado simples, armazena o valor para ser transformado em DataFrame no final da recursao.
+
+    :param data: O dicionario contendo os dados a serem transformados.
+    :type data: dict
+    :param src_key: Chave da camada superior para nomear as chaves no dicionario de saida.
+    :type src_key: str, opcional
+    :return: Um dicionario onde as chaves sao nomes de tabelas e os valores sao DataFrames do Pandas.
+    :rtype: dict
+
+    :TODOS:
+        - Passar todos parametros fixos (como o 'ibge' hardcoded) para o arquivo config/params e ler eles usando as funcoes do yaml
+        - Efetuar outras transformacoes antes de salvar no .parquet, e.g. o campo "territorio" criou diversos arquivos com apenas uma linha. 
+            Apos a criacao de todos DFs, torna-los uma tabela unica com uma coluna identificando o df source.
+    """
     data_t = {}
     data_t_output = {}
 
@@ -48,31 +72,59 @@ def transform_by_type(data:dict, src_key:str = None):
 
     df = pd.DataFrame(data_t, index=[0])
 
-    # TODO IBGE as param later
     key = 'ibge' if src_key is None else key
     data_t_output[key] = df
 
     return data_t_output
 
-# faz o controle dos metadados e invoca a primeira chamada da funcao recursiva que atua sobre os tipos de dados
-def transform(data:dict):
-        
-        data_t = transform_by_type(data)
 
-        return data_t
+def transform(data:dict):
+    """
+    Inicia o processo de transformacao de dados aninhados para DataFrames.
+
+    Esta funcao atua como um wrapper para a função `transform_by_type`,
+    fazendo a chamada inicial para converter a estrutura de dados complexa
+    em um dicionario de DataFrames.
+
+    :param data: O dicionario com os dados a serem transformados.
+    :type data: dict
+    :return: Um dicionario onde as chaves sao nomes de tabelas e os valores
+             sao DataFrames do Pandas.
+    :rtype: dict
+    """   
+
+    data_t = transform_by_type(data)
+
+    return data_t
 
 # gera os metadados, adicina em cada linha que foi listada e salva em um arquivo parquet
 # uma chave unica de ingestao e data tambem sao criados para ter um controle dos itens que foram transformados juntos
     # acho que o nome ingestao seria mais apropriado na etapa do extract, a chave em questao aqui seria para relacionar entre os diversos arquivos parquet para fim de joins
 def load(data:dict):
+    """
+   Salva como arquivos Parquet.
 
-        ingestion_id = str(uuid4())
-        ingestion_date = datetime.now().isoformat()
-        metadata = {"ingestion_id": ingestion_id, "ingestion_date": ingestion_date}
-        df_metadata = pd.DataFrame(metadata, index=[0])
+    Esta funcao itera sobre um dicionario de DataFrames, adiciona metadados
+    como um ID de ingestao e a data/hora da execucao. Em seguida, salva cada
+    DataFrame em um arquivo Parquet no diretorio de saída.
 
-        for k, v in data.items():
-            #TODO to params
-            output_path = f'output/{k}.parquet'
-            df = pd.concat([v, df_metadata], axis=1)
-            df.to_parquet(output_path, index=False)
+    :param data: Um dicionario onde as chaves sao nomes de tabelas e os valores
+                 sao DataFrames do Pandas.
+    :type data: dict
+    :raises Exception: Qualquer excecao levantada pela funcao `to_parquet`.
+
+    :TODOS:
+        - Mandar o tipo de arquivo por parametro (parquet, csv, etc)
+        - Passar parametros fixos (como filepath) para o arquivo config/params e ler eles usando as funcoes do yaml
+    """
+
+    ingestion_id = str(uuid4())
+    ingestion_date = datetime.now().isoformat()
+    metadata = {"ingestion_id": ingestion_id, "ingestion_date": ingestion_date}
+    df_metadata = pd.DataFrame(metadata, index=[0])
+
+    for k, v in data.items():
+        #TODO to params
+        output_path = f'output/{k}.parquet'
+        df = pd.concat([v, df_metadata], axis=1)
+        df.to_parquet(output_path, index=False)
--- src/bot/loader.py
+++ src/bot/loader.py
@@ -9,19 +9,51 @@ endpoint_path = {
 }
 
 # utilizada para ler o arquivo de configuracao com o endpoint
-def read_yaml(path:str, mode:str)->dict:
+def read_yaml(path:str, mode:str='r')->dict:   
+    """
+    Le e carrega dados de um arquivo YAML.
+
+    A funcao abre um arquivo YAML no caminho especificado e o carrega
+    com seguranca para um dicionario Python. Util para ler arquivos
+    de configuracao.
+
+    :param path: O caminho para o arquivo YAML a ser lido.
+    :type path: str
+    :param mode: O modo de abertura do arquivo.
+    :type mode: str, opcional.
+    :return: Um dicionario contendo os dados do arquivo YAML.
+    :rtype: dict
+    """
     with open(path, mode) as f:
         endpoint = yaml.safe_load(f)
     return endpoint
 
 # gera a url a partir do arquivo de configuracao
 # TODO: iteracao na main pro caso de precisar buscar mais dados em paths ou links diferentes
-def generate_url(endpoint_path:dict=endpoint_path)->(str,str,dict): #TODO usar KWARGS
-    # gerando o links a partir do yaml
+def generate_url(endpoint_path:dict=endpoint_path)->(str,str,dict):    
+    """
+    Gera uma URL de uma API a partir de um arquivo YAML.
+
+    Esta funcao le um arquivo de configuração YAML para extrair o protocolo,
+    host, caminho e parâmetros de uma API. Ela constroi a URL completa e
+    retorna a URL, os parametros e o dicionario de configuracao completo.
+
+    :param endpoint_path: Dicionario contendo o caminho e o modo para o arquivo YAML.
+                          O padrao e usar a variáavel global `endpoint_path`.
+    :type endpoint_path: dict, opcional
+    :return: Uma tupla contendo a URL construida, os parametros da API e o dicionario
+             completo de configuracao.
+    :rtype: tuple[str, dict, dict]
+
+    :TODOS:
+        - Retornar dicionario
+        - Iteracao na main para buscar mais links ou paths, caso existam.
+    """
+
     endpoint_params = read_yaml(endpoint_path.get("file"), endpoint_path.get("mode"))
     protocol =  endpoint_params["api"]["protocol"]
     host = endpoint_params["api"]["host"]
-    # TODO iteracao 
+     
     path = endpoint_params["api"]["endpoints"]["get"]["path"]
     params =  endpoint_params["api"]["endpoints"]["get"]["params"]
     url = f'{protocol}://{host}{path}'
